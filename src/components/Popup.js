ddd823831e6c3272f58ff62882020d336babb377
Anthony qin <qin2500@gmail.com>
2022-09-09 20:34:08 -0400

    FINALLY MADE THE POPUPS FADE IN!!!

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -8,14 +8,25 @@ function Popup(props) {
       if(props.isActive)
       {
         document.body.style.overflow = 'hidden';
-        document.getElementById('popup').style.opacity = '1';
+        var popupElement = document.getElementById('popup');
+        popupElement.style.animation = 'visible';
+        popupElement.style.animationDuration = '0.5s';
+        popupElement.style.animationFillMode = 'forwards';
+
       } 
   }, [props.isActive]);
 
   return (props.isActive) ? (
     <div className="popup" id='popup'>
         <div className="popup-inner" >
-            <button className="close-button" onClick={() => {props.setTrigger(false); document.body.style.overflow = 'initial';}}>
+            <button className="close-button" onClick={() => {props.setTrigger(false); document.body.style.overflow = 'initial';
+                    var popupElement = document.getElementById('popup');
+                    setTimeout(function() {
+                      
+                      popupElement.style.animation = 'notVisible';
+                      popupElement.style.animationDuration = '0.5s';
+                      popupElement.style.animationFillMode = 'forwards';},500);
+                    }}>
                 <img className="x-icon" src={x}/>
             </button>
             { props.children }
