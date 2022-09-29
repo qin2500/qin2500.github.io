d392631ae090e89b96d17950ff7b89e1950b1d45
qin2500 <52422134+qin2500@users.noreply.github.com>
2022-09-29 19:49:25 -0400

    Fixed miner animation issues

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -15,8 +15,8 @@ function banner() {
 
                 <TrackVisibility once>
                 {({isVisible}) => 
-                  <div className={isVisible ? "animate__animated animate__fadeInDown" : ""}>
-                   <div className={isVisible ? "" : "no-opacity"}>
+                  <div className={"animate__animated animate__fadeInDown"}>
+                   <div >
                       <h1>Hi, I'm Anthony Qin</h1>
                     <p>A second-year undergraduate at the University of Toronto studying Computer Science specializing in Software Engineering. I aspire for a career where I can develop engaging software and channel my creativity by solving problems in interesting ways. Currently, I am looking for co-op opportunities for the upcoming 2023 winter term. When I'm not at the computer, I enjoy snowboarding, biking, and playing card games with my friends.</p>
                     <a href="mailto:qin2500@gmail.com" target="_blank" rel="noopener noreferrer">
