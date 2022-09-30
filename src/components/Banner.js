a586d07668d0376f60061ceb2effaaa237b2a6fc
qin2500 <52422134+qin2500@users.noreply.github.com>
2022-09-29 20:06:08 -0400

    Fixed Miner error with ultra wide screens

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -19,7 +19,7 @@ function banner() {
                    <div >
                       <h1>Hi, I'm Anthony Qin</h1>
                     <p>A second-year undergraduate at the University of Toronto studying Computer Science specializing in Software Engineering. I aspire for a career where I can develop engaging software and channel my creativity by solving problems in interesting ways. Currently, I am looking for co-op opportunities for the upcoming 2023 winter term. When I'm not at the computer, I enjoy snowboarding, biking, and playing card games with my friends.</p>
-                    <a href="mailto:qin2500@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <a href="mailto:anthony.qin@mail.utoronto.ca" target="_blank" rel="noopener noreferrer">
                       <button className="vvd" > 
                           <span className="button-text">Contact Me</span>
                           <span className="button-helper"></span>
