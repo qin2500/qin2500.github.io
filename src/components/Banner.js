30b78650471bb7194440c83f5c0548aace9b646b
qin2500 <52422134+qin2500@users.noreply.github.com>
2022-12-11 15:04:37 -0500

    Update Status

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,7 +18,7 @@ function banner() {
                   <div className={"animate__animated animate__fadeInDown"}>
                    <div >
                       <h1>Hi, I'm Anthony Qin</h1>
-                    <p>A second-year undergraduate at the University of Toronto studying Computer Science specializing in Software Engineering. I aspire for a career where I can develop engaging software and channel my creativity by solving problems in interesting ways. Currently, I am looking for co-op opportunities for the upcoming 2023 winter term. When I'm not at the computer, I enjoy snowboarding, biking, and playing card games with my friends.</p>
+                    <p>A second-year undergraduate at the University of Toronto studying Computer Science specializing in Software Engineering. I aspire for a career where I can develop engaging software and channel my creativity by solving problems in interesting ways. Currently, I have a co-op position at Manulife from January to April of 2023 but I am always intrested in future opportunities so feel free to reach out. When I'm not at the computer, I enjoy snowboarding, biking, and playing card games with my friends.</p>
                     <a href="mailto:anthony.qin@mail.utoronto.ca" target="_blank" rel="noopener noreferrer">
                       <button className="vvd" > 
                           <span className="button-text">Contact Me</span>
